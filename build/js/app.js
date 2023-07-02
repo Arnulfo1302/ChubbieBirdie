27627687717a192be532cd8fda3d8e7d191eab6d
Arnulfo1302 <118243645+Arnulfo1302@users.noreply.github.com>
2023-07-02 02:31:56 -0600

    Prueba

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,21 @@
-document.addEventListener("DOMContentLoaded",(function(){var e,t=document.querySelector(".activador-barra"),a=document.querySelector(".portafolio-barra");t.addEventListener("mouseenter",(function(){clearTimeout(e),a.classList.add("barra-activa")})),t.addEventListener("mouseleave",(function(){e=setTimeout((function(){a.classList.remove("barra-activa")}),200)})),a.addEventListener("mouseenter",(function(){clearTimeout(e)})),a.addEventListener("mouseleave",(function(){e=setTimeout((function(){t.matches(":hover")||a.classList.remove("barra-activa")}),200)}))}));
+document.addEventListener("DOMContentLoaded", function () {
+  var e,
+    t = document.querySelector(".activador-barra"),
+    a = document.querySelector(".portafolio-barra");
+  t.addEventListener("mouseenter", function () {
+    clearTimeout(e), a.classList.add("barra-activa");
+  }),
+    t.addEventListener("mouseleave", function () {
+      e = setTimeout(function () {
+        a.classList.remove("barra-activa");
+      }, 200);
+    }),
+    a.addEventListener("mouseenter", function () {
+      clearTimeout(e);
+    }),
+    a.addEventListener("mouseleave", function () {
+      e = setTimeout(function () {
+        t.matches(":hover") || a.classList.remove("barra-activa");
+      }, 200);
+    });
+});
